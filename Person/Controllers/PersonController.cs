34eb60d9d8fa98572097d8c2390bafeeb19bd672
simaophil <simaophil@groundrunning.co.uk>
2018-05-11 20:37:14 +0800

    Skeleton client and server without connection between them

---
+++ Person/Controllers/PersonController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Person.Controllers
+{
+    public class PersonController : ApiController
+    {
+        public IEnumerable<string> GetAllPersons()
+        {
+            return new[] { "Simao", "Ronald", "Jan", "Karel" };
+        }
+    }
+}
---
+++ Person/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Person.WebApiApplication" Language="C#" %>
